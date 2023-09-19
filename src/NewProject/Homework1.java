94c321053551890388af4c9103d20bac85b9a92f
zkartal <kartalzafer52@gmail.com>
2023-09-19 16:22:18 +0200

    "Local'de olusturulan Homework1 yeni branchda updated edildi")

--- src/NewProject/Homework1.java
+++ src/NewProject/Homework1.java
@@ -5,5 +5,7 @@ public class Homework1 {
     public static void main(String[] args) {
 
         System.out.println("Local'de Yeni bir Class olusturuldu");
+
+        // Homework1 yeni branchda updated edildi
     }
 }
