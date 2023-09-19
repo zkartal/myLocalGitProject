3d4e35c7664df55915b3027d6e4b8575974162b3
Zafer Kartal <94411448+zkartal@users.noreply.github.com>
2023-09-19 16:13:39 +0200

    Update Homework1.java

--- src/NewProject/Homework1.java
+++ src/NewProject/Homework1.java
@@ -4,5 +4,7 @@ public class Homework1 {
     public static void main(String[] args) {
 
         System.out.println("Local'de Yeni bir Class olusturuldu");
+
+        // Github'da Projeye eklemeler oldu
     }
 }
