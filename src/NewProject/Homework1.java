73961fbd2268020a4010cbc9f49f001e852469fc
zkartal <kartalzafer52@gmail.com>
2023-09-19 16:47:54 +0200

    "Branchta yeni satir eklendi")

--- src/NewProject/Homework1.java
+++ src/NewProject/Homework1.java
@@ -1,14 +1,12 @@
 package NewProject;
 
 public class Homework1 {
-
     public static void main(String[] args) {
 
         System.out.println("Local'de Yeni bir Class olusturuldu");
 
         // Homework1 yeni branchda updated edildi
-        // Branchta yeni satir eklendi
-
+        // yeni satir
 
     }
 }
