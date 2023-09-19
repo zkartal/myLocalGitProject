b513393c00e6a545bfc0e6c1bcd13f1b407e32dd
zkartal <kartalzafer52@gmail.com>
2023-09-19 14:33:43 +0200

    "Homework Project Added Locally !!!")

--- src/NewProject/Homework.java
+++ src/NewProject/Homework.java
@@ -4,5 +4,6 @@ public class Homework {
     public static void main(String[] args) {
 
         System.out.println("Talha Hocanin ilk Ödevi");
+        System.out.println("Homework Project Added Locally !!!");
     }
 }
